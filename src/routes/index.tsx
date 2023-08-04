deb23a9c84a2d60131688b454dab3b2ae1f6472a
Daniel Zotti <daniel.zotti87@gmail.com>
2023-08-04 11:01:48 +0200

    MVP

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,15 +2,25 @@ import {$, component$, useSignal, useVisibleTask$} from "@builder.io/qwik";
 import type {DocumentHead} from "@builder.io/qwik-city";
 import styles from './index.module.scss';
 
+const AUTODECREMENT_DEFAULT_VALUE = "true";
 const COUNTER_DEFAULT_VALUE = 5;
-const LOCAL_STORAGE_KEY = "doomsday_counter"
+const LOCAL_STORAGE_COUNTER_KEY = "doomsday_counter"
+const LOCAL_STORAGE_AUTODECREMENT_KEY = "doomsday_autodecrement"
 
 export default component$(() => {
     const count = useSignal<number | undefined>();
     const isDoomsday = useSignal<boolean>(false);
-    const isDecrementOnLoadActive = useSignal<boolean>(false);
+    const isAutodecrement = useSignal<boolean | undefined>();
     const inputRef = useSignal<HTMLInputElement | undefined>();
     const dialogRef = useSignal<HTMLDialogElement | undefined>();
+
+    const openDialog = $(() => {
+        dialogRef.value?.showModal();
+    })
+
+    const closeDialog = $(() => {
+        dialogRef.value?.close();
+    })
 
     const decrement = $(() => {
         if (!count.value) {
@@ -24,27 +34,31 @@ export default component$(() => {
     });
 
     const setInputValue = $((input?: HTMLInputElement) => {
-        count.value = input?.value ? input.value as unknown as number : 100;
-    })
-
-    const reset = $(() => {
-        count.value = COUNTER_DEFAULT_VALUE
+        count.value = input?.value ? input.value as unknown as number : COUNTER_DEFAULT_VALUE;
         isDoomsday.value = false
     })
 
-    const openDialog = $(() => {
-        dialogRef.value?.showModal()
+    const toggleDecrementOnLoad = $(() => {
+        isAutodecrement.value = !isAutodecrement.value;
     })
 
-    const closeDialog = $(() => {
-        dialogRef.value?.close()
+
+    const reset = $(() => {
+        count.value = COUNTER_DEFAULT_VALUE;
+        isAutodecrement.value = Boolean(AUTODECREMENT_DEFAULT_VALUE);
+        isDoomsday.value = false;
+        closeDialog();
     })
+
 
     useVisibleTask$(() => {
-        const savedCountValue = window.localStorage.getItem(LOCAL_STORAGE_KEY)
-        console.log({savedCountValue})
+        const savedCountValue = window.localStorage.getItem(LOCAL_STORAGE_COUNTER_KEY)
+        const autodecrement = window.localStorage.getItem(LOCAL_STORAGE_AUTODECREMENT_KEY) || AUTODECREMENT_DEFAULT_VALUE;
+
         count.value = savedCountValue ? +savedCountValue : 5;
-        if (isDecrementOnLoadActive.value) {
+        isAutodecrement.value = autodecrement === "true";
+
+        if (isAutodecrement.value) {
             void decrement()
         }
     });
@@ -54,21 +68,52 @@ export default component$(() => {
         if (!count.value) {
             return;
         }
-        window.localStorage.setItem(LOCAL_STORAGE_KEY, count.value.toString())
+        window.localStorage.setItem(LOCAL_STORAGE_COUNTER_KEY, count.value.toString())
+    });
+
+    useVisibleTask$(({track}) => {
+        track(() => isAutodecrement.value);
+        if (isAutodecrement.value === undefined) {
+            return;
+        }
+        window.localStorage.setItem(LOCAL_STORAGE_AUTODECREMENT_KEY, String(isAutodecrement.value))
     });
 
 
     return (
         <div class={styles.page}>
-            <div class={styles.settingsContainer}>
-                <button class={styles.settingsOpenButton} onClick$={openDialog}>⚙️</button>
-            </div>
 
             <dialog class={styles.settingsDialog} ref={dialogRef}>
                 <button class={styles.settingsCloseButton} onClick$={closeDialog}>&times;</button>
                 <div class={styles.settingsContent}>
                     <h1>Settings</h1>
+                    <fieldset class={styles.settingsFieldset}>
+                        <div>
+                            <input id="checkbox-autodecrement"
+                                   type="checkbox"
+                                   checked={isAutodecrement.value}
+                                   onInput$={() => {
+                                       toggleDecrementOnLoad()
+                                   }}
+                            />
+                            <label for="checkbox-autodecrement">
+                                autodecrement on start
+                            </label>
+                        </div>
 
+                        <div>
+                            <input type="number" ref={inputRef} value={count.value}/>
+                            <button onClick$={() => setInputValue(inputRef.value)}>SET</button>
+                        </div>
+                    </fieldset>
+
+                    <div style="text-align:center;">
+                        OR
+                        <br/>
+                        <br/>
+                        <button onClick$={reset}>Restart!</button>
+                        <p><small>(with default values)</small></p>
+                    </div>
                 </div>
             </dialog>
 
@@ -77,16 +122,15 @@ export default component$(() => {
                     <button class={styles.redButton} onClick$={decrement}>{count.value}</button>
                 </div>}
 
-                {isDoomsday.value && <div>
-                    <h1>Boooooom</h1>
-                    <button onClick$={reset}>Restart!</button>
+                {isDoomsday.value && <div class={styles.doomsday}>
+                    <button onClick$={reset}>Restart <small>(with default values)</small></button>
                 </div>}
-
-                <fieldset>
-                    <input type="number" ref={inputRef} value={COUNTER_DEFAULT_VALUE}/>
-                    <button onClick$={() => setInputValue(inputRef.value)}>SET</button>
-                </fieldset>
             </div>
+
+            <div class={styles.settingsContainer}>
+                <button class={styles.settingsOpenButton} onClick$={openDialog}>⚙️</button>
+            </div>
+
         </div>
     );
 });
