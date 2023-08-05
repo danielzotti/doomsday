80aba1fbe8d0a2309cef3a318259d733fa15745b
Daniel Zotti <daniel.zotti87@gmail.com>
2023-08-05 14:55:53 +0200

    Add animations

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,17 @@
 import {$, component$, useSignal, useVisibleTask$} from "@builder.io/qwik";
 import type {DocumentHead} from "@builder.io/qwik-city";
 import styles from './index.module.scss';
+import boomSound from '../../public/audio/boom.mp3'
 
 const AUTODECREMENT_DEFAULT_VALUE = "true";
 const COUNTER_DEFAULT_VALUE = 5;
 const LOCAL_STORAGE_COUNTER_KEY = "doomsday_counter"
+const LOCAL_STORAGE_COUNTER_START_KEY = "doomsday_counter_start"
 const LOCAL_STORAGE_AUTODECREMENT_KEY = "doomsday_autodecrement"
 
 export default component$(() => {
     const count = useSignal<number | undefined>();
+    const startCount = useSignal<number | undefined>();
     const isDoomsday = useSignal<boolean>(false);
     const isAutodecrement = useSignal<boolean | undefined>();
     const inputRef = useSignal<HTMLInputElement | undefined>();
@@ -26,7 +29,7 @@ export default component$(() => {
         if (!count.value) {
             count.value = COUNTER_DEFAULT_VALUE;
         }
-        count.value--
+        count.value--;
         if (count.value <= 0) {
             console.log("It's doomsday!")
             isDoomsday.value = true;
@@ -35,6 +38,7 @@ export default component$(() => {
 
     const setInputValue = $((input?: HTMLInputElement) => {
         count.value = input?.value ? input.value as unknown as number : COUNTER_DEFAULT_VALUE;
+        window.localStorage.setItem(LOCAL_STORAGE_COUNTER_START_KEY, count.value.toString())
         isDoomsday.value = false
     })
 
@@ -44,23 +48,35 @@ export default component$(() => {
 
 
     const reset = $(() => {
-        count.value = COUNTER_DEFAULT_VALUE;
-        isAutodecrement.value = Boolean(AUTODECREMENT_DEFAULT_VALUE);
+        const startValue = window.localStorage.getItem(LOCAL_STORAGE_COUNTER_START_KEY);
+        count.value = startValue ? +startValue : COUNTER_DEFAULT_VALUE;
         isDoomsday.value = false;
         closeDialog();
     })
 
 
     useVisibleTask$(() => {
-        const savedCountValue = window.localStorage.getItem(LOCAL_STORAGE_COUNTER_KEY)
+        const startValue = window.localStorage.getItem(LOCAL_STORAGE_COUNTER_START_KEY) || COUNTER_DEFAULT_VALUE;
+        window.localStorage.setItem(LOCAL_STORAGE_COUNTER_START_KEY, startValue.toString())
+
+        const savedCountValue = window.localStorage.getItem(LOCAL_STORAGE_COUNTER_KEY) || startValue;
         const autodecrement = window.localStorage.getItem(LOCAL_STORAGE_AUTODECREMENT_KEY) || AUTODECREMENT_DEFAULT_VALUE;
 
-        count.value = savedCountValue ? +savedCountValue : 5;
+        startCount.value = +startValue;
+        count.value = +savedCountValue;
         isAutodecrement.value = autodecrement === "true";
 
         if (isAutodecrement.value) {
             void decrement()
         }
+    });
+
+    useVisibleTask$(({track}) => {
+        track(() => startCount.value);
+        if (!startCount.value) {
+            return;
+        }
+        window.localStorage.setItem(LOCAL_STORAGE_COUNTER_START_KEY, startCount.value.toString())
     });
 
     useVisibleTask$(({track}) => {
@@ -102,7 +118,7 @@ export default component$(() => {
                         </div>
 
                         <div>
-                            <input type="number" ref={inputRef} value={count.value}/>
+                            <input type="number" min="1" step="1" ref={inputRef} value={startCount.value}/>
                             <button onClick$={() => setInputValue(inputRef.value)}>SET</button>
                         </div>
                     </fieldset>
@@ -112,7 +128,6 @@ export default component$(() => {
                         <br/>
                         <br/>
                         <button onClick$={reset}>Restart!</button>
-                        <p><small>(with default values)</small></p>
                     </div>
                 </div>
             </dialog>
@@ -123,7 +138,11 @@ export default component$(() => {
                 </div>}
 
                 {isDoomsday.value && <div class={styles.doomsday}>
-                    <button onClick$={reset}>Restart <small>(with default values)</small></button>
+                    <audio controls autoPlay>
+                        <source src={'/audio/boom.mp3'} type="audio/mpeg"/>
+                    </audio>
+                    <div class={styles.doomsdayImage}></div>
+                    <button class={styles.doomsdayRestartButton} onClick$={reset}>Restart</button>
                 </div>}
             </div>
 
